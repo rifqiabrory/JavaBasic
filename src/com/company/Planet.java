79246555fb39cfb23c69e07b008c8575b4598329
Rifqi Abrory <rifqi.upm@gmail.com>
2019-11-27 14:56:13 +0700

    Planet

---
+++ src/com/company/Planet.java
@@ -0,0 +1,14 @@
+public class Planet {
+    private String name;
+    private double mass;
+
+    public Planet(String name, double mass){
+        this.name = name;
+        this.mass = mass;
+    }
+
+    @Override
+    public String toString() {
+        return "Planet "+name+ ", Mass : "+mass;
+    }
+}
